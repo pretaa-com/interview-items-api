7c3a40d4e38e2e6c5c0c10f0491bc53564952f54
Kent Anderson <kent@huml.health>
2026-03-04 11:05:41 -0700

    implement zip function

--- backend/src/sameStructureAs.ts
+++ backend/src/sameStructureAs.ts
@@ -7,26 +7,51 @@
  *   [1, [1, 1]] vs [[2, 2], 2] → false
  */
 export function sameStructureAs(thisArray: unknown[], other: unknown): boolean {
-  // The following conditional is a specific case for the API
-  // it should be removed when the API is fixed to begin
-  // this portion of the interview challenge
-  if (
-    Array.isArray(other) &&
-    thisArray.length === 2 &&
-    other.length === 2 &&
-    !Array.isArray(thisArray[0]) &&
-    !Array.isArray(other[0]) &&
-    Array.isArray(thisArray[1]) &&
-    Array.isArray(other[1]) &&
-    thisArray[1].length === 2 &&
-    other[1].length === 2 &&
-    !Array.isArray(thisArray[1][0]) &&
-    !Array.isArray(thisArray[1][1]) &&
-    !Array.isArray(other[1][0]) &&
-    !Array.isArray(other[1][1])
-  ) {
-    return true;
+  if (!Array.isArray(other)) {
+    return false;
   }
-  // TODO: implement full algorithm for all cases
-  return false;
+
+  const zipped = zipArrays(thisArray, other);
+
+  const result = zipped.reduce((acc, tuple) => {
+    if (tuple.includes(undefined)) {
+      return false;
+    }
+
+    const arrayCount = tuple.filter( element => Array.isArray(element) === true ).length;
+
+    switch(arrayCount) {
+      // Neither side is an array -> same structure
+      case 0: return acc && true;
+
+      // One side is an array -> not same structure
+      case 1: return false;
+
+      // Both sides are arrays -> dig deeper
+      case 2: return acc && sameStructureAs(tuple[0] as unknown[], tuple[1] as unknown[]);
+
+      // Shouldn't happen, but just in case...
+      default:
+        return false;
+    }
+
+  }, true);
+
+  return result;
 }
+
+function zipArrays(
+  firstArray: unknown[],
+  secondArray: unknown[],
+): Array<[unknown | undefined, unknown | undefined]> {
+
+  const maxLength = Math.max(firstArray.length, secondArray.length);
+  const zipped: Array<[unknown|undefined, unknown|undefined]> = [];
+
+  for (let i=0; i < maxLength; i += 1) {
+    zipped.push([firstArray[i], secondArray[i]]);
+  }
+
+  return zipped;
+}
+
